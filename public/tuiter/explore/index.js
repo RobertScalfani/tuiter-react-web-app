7521efee3d26bfa35a6b65022ee5d85977917e39
RobertScalfani <robis345@gmail.com>
2023-02-18 16:43:55 -0500

    refactored old image locations to public

--- public/tuiter/explore/index.js
+++ public/tuiter/explore/index.js
@@ -1,16 +1,16 @@
 import NavigationSidebar from "../NavigationSidebar/index.js";
 import WhoToFollowList from "../WhoToFollowList";
-import PostSummaryList from "../PostSummaryList";
+import ExploreComponent from "./ExploreComponent";
 
-function exploreComponent() {
+function exploreScreen() {
     $('#wd-explore').append(`
        <h2>Explore</h2>
         <div class="row mt-2">
             <div class="col-2 col-md-2 col-lg-1 col-xl-2">
                 ${NavigationSidebar()}
             </div>
-            <div class="col-10 col-lg-7 col-xl-6 bg-primary text-white">
-                ${PostSummaryList()}
+            <div class="col-10 col-lg-7 col-xl-6 text-white">
+                ${ExploreComponent()}
             </div>
             <div class="d-none d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4 text-white">
                 ${WhoToFollowList()}
@@ -19,4 +19,4 @@ function exploreComponent() {
    `);
 }
 
-$(exploreComponent);
+$(exploreScreen);

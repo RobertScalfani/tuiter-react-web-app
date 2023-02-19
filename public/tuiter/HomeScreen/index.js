f63f3d0c57ed04ababb26e5016ab4614daf7fe8a
RobertScalfani <robis345@gmail.com>
2023-02-18 22:29:21 -0500

    link fix

--- public/tuiter/HomeScreen/index.js
+++ public/tuiter/HomeScreen/index.js
@@ -4,7 +4,6 @@ import PostSummaryList from "../PostSummaryList";
 
 function exploreScreen() {
     $('#wd-explore').append(`
-       <h2>Home</h2>
         <div class="row mt-2">
             <div class="col-2 col-md-2 col-lg-1 col-xl-2">
                 ${NavigationSidebar("Home")}

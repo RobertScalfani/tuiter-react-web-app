960fadd632cec90ff51f070526dacd53f8e7828a
RobertScalfani <robis345@gmail.com>
2023-03-26 21:27:20 -0400

    updated edit profile styling:

--- src/tuiter/edit-profile/index.js
+++ src/tuiter/edit-profile/index.js
@@ -72,38 +72,48 @@ const EditProfile = () => {
                 </div>
             </div>
             <div className='mx-3 mb-3'>
-                <form>
-                    <div className="form-group">
-                        <small className="form-text text-muted">
-                            Edit First Name
-                        </small>
-                        <input type="text" className="form-control" placeholder={profile.firstName} onChange={(event) => setFirstName(event.target.value)}/>
+                <form className='form-vertical'>
+                    <div className="form-group pt-3">
+                        <div className='form-control m-0 pt-0 pb-1 px-2 w-100'>
+                            <small className="form-text text-muted">
+                                Edit First Name
+                            </small>
+                            <input type="text" className='form-control border-0 m-0 p-0 w-100' placeholder={profile.firstName} onChange={(event) => setFirstName(event.target.value)}/>
+                        </div>
                     </div>
-                    <div className="form-group">
-                        <small className="form-text text-muted">
-                            Edit Last Name
-                        </small>
-                        <input type="text" className="form-control" placeholder={profile.lastName} onChange={(event) => setLastName(event.target.value)}/>
+                    <div className="form-group pt-3">
+                        <div className='form-control m-0 pt-0 pb-1 px-2 w-100'>
+                            <small className="form-text text-muted">
+                                Edit Last Name
+                            </small>
+                            <input type="text" className="form-control border-0 m-0 p-0 w-100" placeholder={profile.lastName} onChange={(event) => setLastName(event.target.value)}/>
+                        </div>
                     </div>
-                    <div className="form-group">
-                        <small className="form-text text-muted">
-                            Edit Bio
-                        </small>
-                        <textarea type="text" className="form-control" placeholder={profile.bio} onChange={(event) => setBio(event.target.value)} style={{height: "100px"}}/>
+                    <div className="form-group pt-3">
+                        <div className='form-control m-0 pt-0 pb-1 px-2 w-100'>
+                            <small className="form-text text-muted">
+                                Edit Bio
+                            </small>
+                            <textarea type="text" className="form-control border-0 m-0 p-0 w-100" placeholder={profile.bio} onChange={(event) => setBio(event.target.value)} style={{height: "100px"}}/>
+                        </div>
                     </div>
-                    <div className="form-group">
-                        <small className="form-text text-muted">
-                            Edit Location
-                        </small>
-                        <input type="text" className="form-control" placeholder={profile.location} onChange={(event) => setProfileLocation(event.target.value)}/>
+                    <div className="form-group pt-3">
+                        <div className='form-control m-0 pt-0 pb-1 px-2 w-100'>
+                            <small className="form-text text-muted">
+                                Edit Location
+                            </small>
+                            <input type="text" className="form-control border-0 m-0 p-0 w-100" placeholder={profile.location} onChange={(event) => setProfileLocation(event.target.value)}/>
+                        </div>
                     </div>
-                    <div className="form-group">
-                        <small className="form-text text-muted">
-                            Edit Website
-                        </small>
-                        <input type="text" className="form-control" placeholder={profile.website} onChange={(event) => setWebsite(event.target.value)}/>
+                    <div className="form-group pt-3">
+                        <div className='form-control m-0 pt-0 pb-1 px-2 w-100'>
+                            <small className="form-text text-muted">
+                                Edit Website
+                            </small>
+                            <input type="text" className="form-control border-0 m-0 p-0 w-100" placeholder={profile.website} onChange={(event) => setWebsite(event.target.value)}/>
+                        </div>
                     </div>
-                    <div className="form-group">
+                    <div className="form-group pt-3">
                         <button type="button" className='btn p-0 m-0' onClick={() => setEditingBirthDate(true)}>
                             <small className="form-text text-muted mt-5">
                                 Birthdate -
@@ -115,6 +125,12 @@ const EditProfile = () => {
                         {editingBirthDate ?
                             <input type="text" className="form-control" placeholder={profile.dateOfBirth} onChange={(event) => setBirthDate(event.target.value)}/>
                             : <div>{profile.dateOfBirth}</div>}
+                    </div>
+                    <div className='d-flex justify-content-between py-4'>
+                        <span>
+                            Switch to professional
+                        </span>
+                        <i className="bi bi-caret-right text-muted"></i>
                     </div>
                 </form>
             </div>
